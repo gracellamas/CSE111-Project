2e7e2136f60e7dc4d4a619279baa5f8c84c23573
WellsonLau <elwellson@gmail.com>
2020-11-09 16:23:02 -0800

    update table

--- Project/restaurants.sql
+++ Project/restaurants.sql
@@ -1,9 +1,10 @@
 DROP TABLE restaurant;
+DROP TABLE customer;
 DROP TABLE reviews;
 DROP TABLE menu;
 DROP TABLE location;
 DROP TABLE country;
-DROP TABLE continent;
+
 
 -- Create the tables in the schema.
 CREATE TABLE restaurant (
@@ -11,6 +12,12 @@ CREATE TABLE restaurant (
     pricing char(5) not null,
     menukey decimal(3, 0) not null,
     locationkey decimal(3,0) not null,
+    customerkey decimal(3,0) not null
+);
+
+CREATE TABLE customer (
+    name varchar(25) not null,
+    customerkey decimal(3, 0) not null,
     reviewkey decimal(3,0) not null
 );
 
@@ -28,44 +35,43 @@ CREATE TABLE menu (
 
 CREATE TABLE location (
     address varchar(152) not null,
-    locationkey decimal(3,0) not null
+    locationkey decimal(3,0) not null,
+    countrykey decimal(3,0) not null
 );
 
 CREATE TABLE country (
     name char(25) not null,
-    locationkey decimal(3,0) not null,
-    countrykey decimal(3,0) not null,
-    continentkey decimal(3,0) not null
-);
-
-CREATE TABLE continent (
-    name char(25) not null,
-    continentkey decimal(3,0) not null
+    countrykey decimal(3,0) not null
 );
 
 -- Populate every table with the corresponding data.
--- 1.
--- Restaurant
 
--- not sure how to import yet, manually inputting
+-- not sure how to import data yet, manually inputting instead
 -- .import data/restaurant.tbl Restaurant
 
-INSERT INTO restaurant(name, pricing, menukey, locationkey, reviewkey)
+-- Restaurant
+INSERT INTO restaurant(name, pricing, menukey, locationkey, customerkey)
     VALUES ("Restaurant Anzu", "$$$", 1, 1, 1);
+
+-- Customer
+INSERT INTO customer(name, customerkey, reviewkey)
+    VALUES ("Ileyka W.", 1, 1);
+
 -- Reviews
 INSERT INTO reviews(user, stars, reviewkey)
     VALUES ("Ileyka W.", 5, 1);
+
 -- Menu
 INSERT INTO menu(item, cost, menukey)
-    VALUES ("Garlic Chili Edamame", 14, 1);
-INSERT INTO menu(item, cost, menukey)
-    VALUES ("Honey Soy Glazed Chicken Wings", 16, 1);
+    VALUES 
+    ("Garlic Chili Edamame", 14, 1),
+    ("Honey Soy Glazed Chicken Wings", 16, 1);
+
 -- Location
-INSERT INTO location(address, locationkey)
-    VALUES ("222 Mason St, San Francisco, CA 94102", 1);
+INSERT INTO location(address, locationkey, countrykey)
+    VALUES ("222 Mason St, San Francisco, CA 94102", 1, 1);
+
 -- Country
-INSERT INTO country(name, locationkey, countrykey, continentkey)
-    VALUES ("USA", 1, 1, 1);
--- Continent
-INSERT INTO continent(name, continentkey)
-    VALUES ("North America", 1);
+INSERT INTO country(name, countrykey)
+    VALUES ("USA", 1);
+
